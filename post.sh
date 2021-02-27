73c7b397d0478abc6940a70b7d98a60b55ca825e
Oliver Johnson <oa_johnson@outlook.com>
2021-02-27 06:34:16 -0600

    Made script

---
+++ post.sh
@@ -0,0 +1,39 @@
+sudo apt-get update
+
+# Install Developer Tools
+sudo apt-get install vim \
+	tmux \
+	tig \
+	htop \
+	apt-transport-https \
+	ca-certifacates \
+	curl \
+	gnupg-agent \
+	software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) \
+	stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo snap install code discord bitwarden chromium
+
+# Install Media tools
+sudo apt-get vlc ffmpeg mcomix pinta darktable youtube-dl
+
+# Install Learning tools
+sudo apt-get gjiten kanjipad
+
+# Docker Post Installation
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+echo "To finish Docker setup, log out and log back in and run the following: \
+	\nnewgrp docker \
+	\nThen verify with docker run hello-world"
+
